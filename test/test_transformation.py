3a1ae8626aabc9ef2519ef81278c8a41c9f23b6a
leorud <leorud@bgs.ac.uk>
2023-08-02 14:17:49 +0100

    Add recoordinate SEM CSV file test

--- test/test_transformation.py
+++ test/test_transformation.py
@@ -1,9 +1,14 @@
 """
 Tests for functions in the transformation module
 """
-import numpy as np
 import pytest
 
+import numpy as np
+
+from csv import DictReader
+from pathlib import Path
+
+from tactool.main import TACtool
 from tactool.analysis_point import (
     affine_transform_point,
     affine_transform_matrix,
@@ -55,3 +60,47 @@ def test_affine_transform_matrix():
 
     # Assert
     np.testing.assert_array_almost_equal(matrix, expected, decimal=10)
+
+
+def test_recoordinate_sem_points(tmp_path: Path, tactool: TACtool):
+    # Arrange
+    input_csv = "test/data/analysis_points_complete.csv"
+    output_csv = tmp_path / "analysis_points_complete.csv"
+    # Place 3 Analysis Points which will be used for recoordination
+    tactool.graphics_view.left_click.emit(336, 472)
+    tactool.graphics_view.left_click.emit(318, 394)
+    tactool.graphics_view.left_click.emit(268, 469)
+    # Toggle recoordinate dialog so that the recoordinate_dialog is callable
+    tactool.window.toggle_recoordinate_dialog()
+    expected_coordinates = [
+        ("335", "472"),
+        ("317", "394"),
+        ("267", "469"),
+        ("339", "526"),
+        ("379", "362"),
+    ]
+
+    # Act
+    tactool.recoordinate_dialog.recoordinate_sem_points(
+        input_csv, output_csv,
+        x_header="X", y_header="Y", ref_col="Type", ref_label="RefMark",
+    )
+
+    # Assert
+    with open(input_csv) as input_file, open(output_csv) as output_file:
+        input_reader = DictReader(input_file)
+        output_reader = DictReader(output_file)
+
+        # Check that the headers remain the same
+        assert input_reader.fieldnames == output_reader.fieldnames
+
+        non_coord_headers = ["Name", "Type", "diameter", "scale", "colour", "mount_name", "material", "notes"]
+
+        # Iterate through CSV file lines
+        for input_item, output_item, (new_x, new_y) in zip(input_reader, output_reader, expected_coordinates):
+            # Check that the non coordinate fields remain the same
+            for header in non_coord_headers:
+                assert input_item[header] == output_item[header]
+            # Check that the coordinate fields are correct
+            assert output_item["X"] == new_x
+            assert output_item["Y"] == new_y
